ee8e11498b18a9f9b5783015a9d14a486f44b81d
Abagail Hudson <abagailanne@gmail.com>
2019-09-08 21:34:24 +0000

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+    def index
+      @cart = cart
+    end
+
+    def add
+        session[:cart] << params[:product_name]
+        redirect_to root_path
+    end
+  end
