fe8a486467185b2e80480e70cb3fdd257b5ca544
Becky Phan <beckyphan@live.com>
2019-10-15 20:35:19 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  # created root route '/' to products#index
+  root 'products#index'
+
+  # created post route to products#add
+  post 'products/add'
+
+  # Prefix Verb URI Pattern             Controller#Action
+  #         root GET  /                       products#index
+  # products_add POST /products/add(.:format) products#add
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
