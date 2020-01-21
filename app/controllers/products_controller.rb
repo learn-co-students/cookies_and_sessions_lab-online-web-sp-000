8fd591e419345cc453e19f3223909ec5222e80c7
rich0024 <yance0022@gmail.com>
2020-01-21 09:15:32 -0800

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+class ProductsController < ApplicationController
+    
+    def index
+        @cart = cart
+    end
+
+    def create
+        @cart = cart
+        @cart << params[:product]
+        session[:cart] = @cart
+        render "index"
+    end
+end 
