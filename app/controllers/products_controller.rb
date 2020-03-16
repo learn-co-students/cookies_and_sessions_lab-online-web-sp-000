63a4869f63345e34347da6f5d29228a40aa64733
indre93 <indre.bakeer@gmail.com>
2020-03-16 15:10:10 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,12 @@
 class ProductsController < ApplicationController
-
+  
   def index
 
   end
 
   def add
     cart << params[:product]
-    redirect_to '/'
+    redirect_to root_path
   end
-
+  
 end
