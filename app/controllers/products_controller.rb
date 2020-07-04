e70a4a34150897b8c7fb820a3b5c27b5efdf5481
gabejoseph <gabriel.joseph4@gmail.com>
2020-07-04 17:53:51 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class ProductsController < ApplicationController
+
+    def index
+
+    end 
+
+    def add 
+        cart << params[:product]
+        render :index
+    end 
+
+
+end
