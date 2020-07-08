2111b349fc3b2d5341c6929cc418633f4fe40aba
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-08 19:43:04 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,12 +2,11 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all
   end
 
   def add
-    @product = Product.find(params[:id])
-    cart << @product.id
+    cart << params[:product]
+    render :index
   end
-  
+
 end
