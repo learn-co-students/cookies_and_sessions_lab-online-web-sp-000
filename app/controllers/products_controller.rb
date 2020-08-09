ba3f13eb56503a0bf14a48ee200f6fa739ab3e3c
ShaylaWhite <shayladwhite1@gmail.com>
2020-08-09 14:27:06 -0700

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,16 @@
+class ProductsController < ApplicationController
+
+    def index
+      @cart = cart
+    end
+  
+    def add
+    
+      @cart = cart
+      @cart << params[:product]
+      session[:cart] = @cart
+      redirect_to '/'
+    end
+  
+  end
+  
