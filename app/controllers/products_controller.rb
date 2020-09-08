1085dbbfffb70307c0e9a22cb41b875159204f2a
The-Gentleman <zraja4402@gmail.com>
2020-09-08 19:57:06 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,7 @@ class ProductsController < ApplicationController
     end 
 
     def add 
+        cart << params["product"]
+        render :index
     end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :products, only: [:index]
   get '/', to: 'products#index'
+  post '/add', to: 'products#add'
 end
