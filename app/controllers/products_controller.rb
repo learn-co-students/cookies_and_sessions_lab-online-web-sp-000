1aa8c4540b6a5bc72939f06e6a8425db3a3b67dc
Yumacorn <ReachRyanMui@gmail.com>
2020-10-05 00:05:59 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,19 @@
 class ProductsController < ApplicationController
     def index
         @products = Product.all
+        @product = Product.new
     end
 
     def new
         @products = Product.new
     end
+
+    def create
+        @product = Product.new
+        @product.save
+        @products = Product.all
+
+        redirect_to '/'        
+    end
 end
+
