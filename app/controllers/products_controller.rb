1b77db03e5add93e9d58aae7c68629f1c4713da0
mdacht777 <mdacht777@gmail.com>
2020-10-10 09:25:22 -0600

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,19 @@
+class ProductsController < ApplicationController #inherits from ...
+end
+
+def index
+    binding.pry
+    @cart = cart
+end
+
+def show
+end
+
+def create
+end
+
+def update
+end
+
+def destroy
+end
