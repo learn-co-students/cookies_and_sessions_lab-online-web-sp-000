fc00b6c29084afe51bcf40289df15b4e790daaa0
AustinRhoads <austinrrhoads@ymail.com>
2020-10-13 18:38:18 -0500

    done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,22 @@
 class ProductsController < ApplicationController
 
    def index
-    #binding.pry
     @cart = cart
-
+   
     end
 
     def add
+        
         @cart = cart
+        
         @product = params[:product]
         
         @cart << @product
         session[:cart] = @cart
-      
+      render :index
 
     end
 
+   
   
 end
