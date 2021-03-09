f3797cdcd8d01f8ca1ec6c0ec4b92db68a1cf1ed
RSigmaSG <sg.comparch@gmail.com>
2021-03-09 01:03:42 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,7 @@
 class ProductsController < ApplicationController
     def index
-        @cart = session[:cart]
     end
     def add
-        puts params 
-        puts "HEREEEEEEEEEEEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-        puts params[:product]
-        puts "HEREEEEEEEEEEEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
         cart << params[:product]
         redirect_to root_path
     end
