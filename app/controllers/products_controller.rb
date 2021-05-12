5aab0b4a02fc948a8f4fb46a9954de0ced34bf30
Jess Pesale <jess_pesale@aol.com>
2021-05-12 15:36:24 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+
+    def index 
+        @products = Product.all
+    end
+
+    def add
+        @product = Product.find(params[:id])
+
+        cart << @product.id 
+        render :index
+    end
+
+end
