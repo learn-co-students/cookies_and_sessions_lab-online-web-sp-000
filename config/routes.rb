5717ecfcdf8f84efa1efabebaba9c999422b6b04
JohannesHupfeld <j.hupfeld@gmail.com>
2021-06-04 15:53:28 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index' #root is index action in products controlers
+  post '/' => 'products#add'  #(post to root?)add action , posts controller
 end
