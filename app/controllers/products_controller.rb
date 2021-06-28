52ee423a6fa7ef9a09b20c8ac823ca671997a3c4
McKayla Lankau <lankaukk@yahoo.com>
2021-06-28 19:11:29 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,9 @@
 class ProductsController < ApplicationController
   def index
-    @cart = cart
   end
 
   def add
     cart << params[:product]
-    redirect_to root_path
+    render :index
   end
 end
